32200bfe20f1b39ab89861f899f24f96e14c029a
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-03-26 17:31:56 +0530

    hh

--- app.py
+++ app.py
@@ -13,25 +13,32 @@ import pickle
 
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+# prediction function 
+def ValuePredictor(to_predict_list): 
+	to_predict = np.array(to_predict_list).reshape(1,-1)
+	print(to_predict)
+	loaded_model = pickle.load(open("model.pkl", "rb")) 
+	result = loaded_model.predict(to_predict) 
+	return result[0] 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/predict', methods = ['POST']) 
+def predict(): 
+	if request.method == 'POST': 
+		to_predict_list = request.form.to_dict()
+		to_predict_list = list(to_predict_list.values())
+		to_predict_list = to_predict_list[:-1]
+		to_predict_list=list(map(float, to_predict_list))
+		result = ValuePredictor(to_predict_list)		 
+		if int(result)== 1: 
+			prediction ='cancer'
+		else: 
+			prediction ='Income less that 50K'			
+		return render_template("index.html", prediction_text =prediction) 
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    return render_template('index.html')
-
-    
 
     
         
 
 if __name__ == "__main__":
-    model = pickle.load(open('model.pkl', 'rb'))
     app.run(debug=True)
 
