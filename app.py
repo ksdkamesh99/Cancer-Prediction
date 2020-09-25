409c8f1bb27efdf9a03c5578b2faa204e9e4170d
Sai Durga Kamesh Kota <ksdkamesh99@gmail.com>
2020-09-25 08:20:24 +0530

    #1 resolved and #4 resolved

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ Original file is located at
 """
 
 import pandas as pd
+import numpy as np
 from flask import Flask, request, render_template
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
